affe438dc1927b08a3968b9c6988ee7b63bf9db0
Maxcell Wilson <maxcell.wilson@gmail.com>
2020-02-27 23:16:37 -0500

    Added models and updated settings

--- flashcards/models.py
+++ flashcards/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+
+class CardBundle(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+
+
+class Card(models.Model):
+    front_text = models.CharField(max_length=500)
+    back_text = models.CharField(max_length=500)
+    bundle = models.ForeignKey(CardBundle, on_delete=models.CASCADE)
